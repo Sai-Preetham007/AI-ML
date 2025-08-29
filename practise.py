f8c92ea8c407799fc6d1c73cc3a5a99aa2a0c6c6
Sai Preetham <saipreethamudumula@gmail.com>
2025-08-29 20:37:13 +0530

    Day-1

---
+++ practise.py
@@ -0,0 +1,21 @@
+# This is a single line comment
+
+'''
+This is a
+multi line comment.
+'''
+
+'''
+Terminal (cmd ---> Command Prompt) :
+
+cls ---> clear the screen
+cd ---> change directory
+mkdir ---> make directory
+rmdir ---> remove directory
+'''
+
+print("Hello $@! **7")
+
+x=5
+
+print("x :", x)
