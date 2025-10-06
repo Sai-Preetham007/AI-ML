ae4f141fe3c84b1eba5578a0b87b0b5c9d8a1ebf
Sai Preetham <saipreetham@Sais-MacBook-Air.local>
2025-10-06 20:02:08 +0530

    File handling and Virtual env

---
+++ practise.py
@@ -0,0 +1,20 @@
+# This is a single line comment
+
+'''
+This is a
+multi line comment.
+'''
+
+'''
+Terminal (cmd ---> Command Prompt) :
+
+cls ---> clear the screen
+cd ---> change directory, cd .. ---> changes to parent level directory from the current directory.
+mkdir ---> make directory
+rmdir ---> remove directory
+'''
+
+print("Hello $@! **7")
+
+x=5
+print("x :", x)
